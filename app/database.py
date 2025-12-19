f7c8c5346c3012744d88b0ac62f1e17fa160e27c
StepanZubok <busyn258@gmail.com>
2025-12-19 20:04:10 +0100

    remove custom service32

--- app/database.py
+++ app/database.py
@@ -2,6 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.config import settings
+import os
+
+
+DATABASE_HOST = os.getenv("DATABASE_HOST", "postgres")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USER = os.getenv("DATABASE_USERNAME", "postgres")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
 
 DATABASE_URL = f"postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}"
 
